7943841b4f820e1910708d47251698945972f0d8
ryanjakeherian <ryancherian@hotmail.com>
2025-03-30 18:27:34 +0100

    Ajusting config for athena

--- examples/athena_train_tiny_llama.sh
+++ examples/athena_train_tiny_llama.sh
@@ -6,15 +6,16 @@
 #SBATCH --cpus-per-task=8               # CPU cores per task
 #SBATCH --mem=32G                       # Memory allocation
 #SBATCH --time=24:00:00                 # Time limit (HH:MM:SS)
-#SBATCH --output=slurm-%j.out           # Standard output log
+#SBATCH --output=~/4YP/slurm_output/nanotron/slurm-%j.out           # Standard output log
 #SBATCH --error=slurm-%j.err            # Error log file
 
 # Load necessary modules (if needed)
 module load cuda/12.4   # Adjust based on your environment
 module load python/3.11  # Adjust based on your environment
 
-# Activate virtual environment (if applicable)
-source ~/myenv/bin/activate
+# >>>> CONDA >>>>
+source ~/miniconda3/etc/profile.d/conda.sh # Init conda necessary as slurm starts with non-interactive shell
+conda activate nanotron
 
 # Run the command
 python3 config_tiny_llama.py && (cd ..; CUDA_DEVICE_MAX_CONNECTIONS=1 torchrun --nproc_per_node=2 run_train.py --config-file examples/config_tiny_llama.yaml)
